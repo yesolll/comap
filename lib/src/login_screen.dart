6d5dbe7481f5a7dbcf41bbcac6a5c8beef995d52
yesolll <9595kys@gmail.com>
2024-08-20 18:02:56 +0900

    [Feat] 로그인 진행중

--- lib/src/login_screen.dart
+++ lib/src/login_screen.dart
@@ -1,9 +1,18 @@
+import 'dart:convert';
+
+import 'package:comap/model/Place.dart';
 import 'package:comap/src/home_screen.dart';
+import 'package:comap/src/place_list_screen.dart';
+import 'package:comap/util/service/apiCall/ApiCallService.dart';
+import 'package:comap/util/service/apiCall/RequestData.dart';
+import 'package:comap/util/service/apiCall/type/ApiCallType.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
@@ -14,22 +23,15 @@ class _LoginScreenState extends State<LoginScreen> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  // Future<void> _showWebView(BuildContext context) {
-  //   return Navigator.of(context)
-  //       .push(MaterialPageRoute(builder: (context) => InputWebView()));
-  // }
-
   Future<void> _login() async {
     if (_formKey.currentState!.validate()) {
       String username = _usernameController.text.trim();
       String password = _passwordController.text.trim();
-
-      // TODO: Replace with your actual login logic
       if (username == 'admin' && password == 'password') {
-        await secureStorage.write(key: 'isLoggedIn', value: 'true');
-        await secureStorage.write(key: 'userName', value: '예쏠');
-        Get.offAll(() => const HomeScreen());
-        // _showWebView(context);
+        // 로그인 API 호출
+        // await secureStorage.write(key: 'isLoggedIn', value: 'true');
+        // await secureStorage.write(key: 'accessToken', value: '받은');
+        Get.offAll(() => const PlaceListScreen());
       } else {
         _showErrorDialog();
       }
@@ -40,12 +42,12 @@ class _LoginScreenState extends State<LoginScreen> {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
-        title: Text('로그인 실패'),
-        content: Text('아이디 또는 비밀번호를 확인해주세요.'),
+        title: const Text('로그인 실패'),
+        content: const Text('아이디 또는 비밀번호를 확인해주세요.'),
         actions: [
           TextButton(
             onPressed: () => Navigator.pop(context),
-            child: Text('확인'),
+            child: const Text('확인'),
           ),
         ],
       ),
@@ -57,8 +59,8 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: ListView(
         children: [
-          SizedBox(height: 70),
-          Text('로그인', style: TextStyle(fontSize: 32), textAlign: TextAlign.center),
+          const SizedBox(height: 70),
+          const Text('로그인', style: TextStyle(fontSize: 32), textAlign: TextAlign.center),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Form(
@@ -67,7 +69,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 children: [
                   TextFormField(
                     controller: _usernameController,
-                    decoration: InputDecoration(labelText: '아이디'),
+                    decoration: const InputDecoration(labelText: '닉네임'),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return '아이디를 입력해주세요.';
@@ -77,7 +79,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                   TextFormField(
                     controller: _passwordController,
-                    decoration: InputDecoration(labelText: '비밀번호'),
+                    decoration: const InputDecoration(labelText: '비밀번호'),
                     obscureText: true,
                     validator: (value) {
                       if (value == null || value.isEmpty) {
@@ -86,7 +88,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       return null;
                     },
                   ),
-                  SizedBox(height: 20),
+                  const SizedBox(height: 20),
                   ElevatedButton(
                     onPressed: _login,
                     child: Text('로그인'),
